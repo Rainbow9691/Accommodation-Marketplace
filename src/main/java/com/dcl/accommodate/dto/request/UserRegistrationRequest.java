d673ff40e498bb4b51c5cfd8699eed556edc1527
Rainbow9691 <raj9691vis@gmail.com>
2025-10-06 22:49:25 +0530

    UserRegistrationRequest And UserRegistrationResponse Updated

--- src/main/java/com/dcl/accommodate/dto/request/UserRegistrationRequest.java
+++ src/main/java/com/dcl/accommodate/dto/request/UserRegistrationRequest.java
@@ -1,4 +1,75 @@
 package com.dcl.accommodate.dto.request;
 
+
+import jakarta.persistence.Column;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import com.dcl.accommodate.ENUMS.UserRoles;
+
+import java.util.Date;
+
 public class UserRegistrationRequest {
+    private String firstName;
+    private String lastName;
+    private Date dateOfBirth;
+    private UserRoles roles;
+    private String email;
+    private String password;
+    private String phoneNumber;
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public Date getDateOfBirth() {
+        return dateOfBirth;
+    }
+
+    public void setDateOfBirth(Date dateOfBirth) {
+        this.dateOfBirth = dateOfBirth;
+    }
+
+    public UserRoles getRoles() {
+        return roles;
+    }
+
+    public void setRoles(UserRoles roles) {
+        this.roles = roles;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
 }
--- src/main/java/com/dcl/accommodate/dto/response/UserRegistrationResponse.java
+++ src/main/java/com/dcl/accommodate/dto/response/UserRegistrationResponse.java
@@ -1,4 +1,95 @@
 package com.dcl.accommodate.dto.response;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+
+import java.time.Instant;
+import java.util.Date;
+import java.util.UUID;
+import com.dcl.accommodate.ENUMS.UserRoles;
+
 public class UserRegistrationResponse {
+
+    private UUID userId;
+    private String firstName;
+    private String lastName;
+    private Date dateOfBirth;
+    private UserRoles roles;
+    private String email;
+    private String avatar;
+    private Instant createdAt;
+    private Instant lastModifiedDate;
+
+    public UUID getUserId() {
+        return userId;
+    }
+
+    public void setUserId(UUID userId) {
+        this.userId = userId;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public Date getDateOfBirth() {
+        return dateOfBirth;
+    }
+
+    public void setDateOfBirth(Date dateOfBirth) {
+        this.dateOfBirth = dateOfBirth;
+    }
+
+    public UserRoles getRoles() {
+        return roles;
+    }
+
+    public void setRoles(UserRoles roles) {
+        this.roles = roles;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getAvatar() {
+        return avatar;
+    }
+
+    public void setAvatar(String avatar) {
+        this.avatar = avatar;
+    }
+
+    public Instant getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(Instant createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public Instant getLastModifiedDate() {
+        return lastModifiedDate;
+    }
+
+    public void setLastModifiedDate(Instant lastModifiedDate) {
+        this.lastModifiedDate = lastModifiedDate;
+    }
 }
